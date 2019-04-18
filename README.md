27298c4d6335eb555a20824624bd184b28269f42
Trevor Morris <trevor.morris@easyfundraising.org.uk>
2019-04-18 12:19:55 +0100

    Editor styling. Added logo.

--- README.md
+++ README.md
@@ -1,26 +1,31 @@
 # ![TrailMaps.co.uk](public/assets/logo.png)
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Run your tests
+
 ```
 npm run test
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <template>
   <div id="app">
-    <h1><a href=""><img src="assets/logo.png" alt="TrailMaps.co.uk GPX" title=""></a></h1>
+    <h1>
+      <a href>
+        <img src="assets/logo.png" alt="TrailMaps.co.uk GPX" title>
+      </a>
+    </h1>
   </div>
 </template>
 
+<script>
+import strava from "strava";
+import gpxParse from "gpx-parse";
+
+export default {
+  name: "TrailMaps",
+  components: {},
+  computed: {},
+  mounted() {
+    console.log(strava);
+    console.log(gpxParse);
+  },
+  methods: {}
+  
+};
+</script>
+
 <style>
-  html, body {
-    padding: 0; margin: 0;
-  }
-  h1 {
-    margin: 20px;
-  }
-  h1 img {
-    display: block;
-    max-width: 120px;
-  }
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+h1 {
+  margin: 20px;
+}
+h1 img {
+  display: block;
+  max-width: 120px;
+}
 </style>
